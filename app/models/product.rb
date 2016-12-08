4b4f010a5b6109f7f1e30296dd3dccd5daa11dca
rezaruslan94 <reza.ruslan94@gmail.com>
2016-12-08 17:20:49 +0700

    show tag to view

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_and_belongs_to_many :tags
+
+  def all_tags=(names)
+    self.tags = names.split(",").map do |name|
+        Tag.where(name: name.strip).first_or_create!
+    end
+  end
+
+  def all_tags
+    self.tags.map(&:name).join(", ")
+  end
 end
